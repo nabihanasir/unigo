5737761e6ca702fc78c2f426f5cbae42c900a682
nabihanasir <146710141+nabihanasir@users.noreply.github.com>
2025-04-30 13:08:42 +0500

    nabiha added navigation

--- App.js
+++ App.js
@@ -1,33 +1,5 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import Login from './screens/login';
-// import LargeButton from './components/button';
-// import Onboarding1 from './screens/onboarding1';
-// import Onboarding2 from './screens/onboarding2';
-// import Onboarding3 from './screens/onboarding3';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//      <Login></Login>
-//      <LargeButton text ="next"></LargeButton>
-//      <onboarding1 />
-//       <onboarding2 />
-//       <onboarding3 />
-//     </View>
-
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Login from './screens/login';
 import LargeButton from './components/button';
@@ -35,23 +7,28 @@ import Onboarding1 from './screens/onboarding1';
 import Onboarding2 from './screens/onboarding2';
 import Onboarding3 from './screens/onboarding3';
 
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator }from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator }from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import './gesture-handler';
+
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
+
+
   return (
-    <View style={styles.container}>
-    {/* ./ <Login />
-      <LargeButton text="next" />
-      <Onboarding1 />
-      <Onboarding2 /> */}
-      <Onboarding1 />
-    </View>
+    
+    <NavigationContainer>
+     <Stack.Navigator initialRouteName='Onboarding1' screenOptions={{ headerShown: false }} >
+      <Stack.Screen name ="Onboarding1" component ={Onboarding1}/>
+      <Stack.Screen name ="Onboarding2" component ={Onboarding2}/>
+      <Stack.Screen name ="Onboarding3" component ={Onboarding3}/>
+     </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/onboarding1.js
+++ screens/onboarding1.js
@@ -1,8 +1,10 @@
 import { View ,Image, Text} from "react-native";
 import LargeButton from '../components/button';
 import GlobalStyles from "../assets/styles";
+import React from "react";
 
-export default function onboarding1(){
+
+export default function onboarding1({navigation}){
     return(
         <>
         <View style={GlobalStyles.onboardingContainer}>
@@ -13,7 +15,7 @@ export default function onboarding1(){
             <Text style={GlobalStyles.onboardingDescription}>
                 Welcome to a new era of university {"\n"}services safety with UniGo. Let's embark {"\n"}on this journey together!
             </Text> 
-            <LargeButton text = 'Next'></LargeButton>
+            <LargeButton text = 'Next' onPress={()=>navigation.navigate('Onboarding2')}></LargeButton>
         </View>
         </>
     )
